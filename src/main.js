a90ead99a433ccc0228763469cdbf29f7a2857d7
Votre Nom <votre_email@example.com>
2025-04-06 13:18:14 +0200

    Club Arcade

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/black.css";
+import "./animation.scss";
+import "./styles.scss";
+import Reveal from "reveal.js";
+
+Reveal.initialize({});
