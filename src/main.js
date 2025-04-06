f178a539639c273d7bcb7bf73291b993dbc97af5
Votre Nom <votre_email@example.com>
2025-04-06 16:37:43 +0200

    FCI

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import "reveal.js/dist/theme/black.css";
 import "./animation.scss";
 import "./clube-arcade.scss";
 import "./idee.scss";
+import "./amphenol.scss";
 import Reveal from "reveal.js";
 
-Reveal.initialize({});
+Reveal.initialize({
+  hash: true,
+});
