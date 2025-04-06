383fb4d364ec6fb8ee82965e54451017a0060c08
Votre Nom <votre_email@example.com>
2025-04-06 13:52:53 +0200

    IDEE numérique

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/black.css";
 import "./animation.scss";
-import "./styles.scss";
+import "./clube-arcade.scss";
+import "./idee.scss";
 import Reveal from "reveal.js";
 
 Reveal.initialize({});
