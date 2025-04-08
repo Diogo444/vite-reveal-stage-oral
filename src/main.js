78a45ec8170f252736c7854e41e3f5f58b258a06
Diogo <da7822000@gmail.com>
2025-04-08 10:49:10 +0200

    Ajout install

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import "./animation.scss";
 import "./clube-arcade.scss";
 import "./idee.scss";
 import "./amphenol.scss";
+<<<<<<< HEAD
+import "./presentation.scss";
+import "./install.scss"
+
+
+=======
+>>>>>>> f178a539639c273d7bcb7bf73291b993dbc97af5
 import Reveal from "reveal.js";
 
 Reveal.initialize({
